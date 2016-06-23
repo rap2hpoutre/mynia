0df0a2e6122452a7e74b64bdbeec9ebb0853a49e
rap2hpoutre <raphaelht@gmail.com>
2016-06-23 13:34:11 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x = [
+        "Ai", "Aba", "Agi", "Ava", "Cami", "Centi", "Cogi", 
+        "Demi", "Diva", "Dyna", "Ea", "Ei", "Fa", "Ge", "Ja", 
+        "I", "Ka", "Kay", "Ki", "Kwi", "La", "Lee", "Mee", 
+        "Mi", "Mu", "My", "Oo", "O", "Oyo", "Pixo", "Pla", 
+        "Qua", "Qui", "Roo", "Rhy", "Ska", "Sky", "Ski", 
+        "Ta", "Tri", "Twi", "Tru", "Vi", "Voo", "Wiki", 
+        "Ya", "Yaki", "Yo", "Za", "Zoo"
+    ];
+    println!("{}", x[0]);
+}
