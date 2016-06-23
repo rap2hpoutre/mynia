66e9ad4e3f020c1792463d46b75a0ef59c9ce47f
rap2hpoutre <raphaelht@gmail.com>
2016-06-23 14:02:17 +0200

    init

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
+extern crate rand;
+
+use rand::Rng;
+
 fn main() {
     let x = [
-        "Ai", "Aba", "Agi", "Ava", "Cami", "Centi", "Cogi", 
-        "Demi", "Diva", "Dyna", "Ea", "Ei", "Fa", "Ge", "Ja", 
-        "I", "Ka", "Kay", "Ki", "Kwi", "La", "Lee", "Mee", 
-        "Mi", "Mu", "My", "Oo", "O", "Oyo", "Pixo", "Pla", 
-        "Qua", "Qui", "Roo", "Rhy", "Ska", "Sky", "Ski", 
-        "Ta", "Tri", "Twi", "Tru", "Vi", "Voo", "Wiki", 
-        "Ya", "Yaki", "Yo", "Za", "Zoo"
+        "Abi", "Ai", "Aba", "Agi", "Ava", "Cami", "Centi", 
+        "Cogi", "Demi", "Diva", "Dyna", "Ea", "Ei", "Fa", 
+        "Fi", "Ge", "Gi", "Gle", "Ja", "Je", "I", "Io", "Ka", 
+        "Kay", "Ki", "Kwi", "La", "Lee", "Mee", "Mi", "Mu", 
+        "My", "Oo", "O", "Oyo", "Pixo", "Pla", "Ple", "Qua", 
+        "Qui", "Roo", "Rhy", "Ska", "Sky", "Ski", "Ta", 
+        "Tri", "Twi", "Tru", "Vi", "Voo", "Wa", "Wiki", "Ya", 
+        "Yaki", "Yo", "Za", "Zoo"
     ];
-    println!("{}", x[0]);
+    let y = [
+        "ba", "bi", "ble", "blee", "boo", "box", "by", "car", 
+        "cero", "deo", "del", "do", "doo", "gen", "go", "jo", 
+        "kwi", "lane", "lia", "lith",  "loo", "lium", "mba", 
+        "mbee", "mbo", "mbu", "mia", "mm", "nder", "ndo", 
+        "ndu", "noodle", "nix", "nte", "nti", "nu", "nyx", 
+        "pe", "re", "ta", "tri", "tz", "va", "vee", "veo", 
+        "via", "vu", "wee", "wi", "xo", "yo", "zz", "zzy", 
+        "zio", "zoo", "zu", "zix"
+    ];
+
+    println!(
+        "Your unicorn name will be: {}{}", 
+        x[rand::thread_rng().gen_range(0, x.len())], 
+        y[rand::thread_rng().gen_range(0, y.len())], 
+    );
 }
